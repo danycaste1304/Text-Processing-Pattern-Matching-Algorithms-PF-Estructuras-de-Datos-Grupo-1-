188dcc81497a1f953ed7385ba57858799e9bbd1c
vanealmagro123 <valmagro@estud.usfq.edu.ec>
2026-04-26 17:40:18 -0500

    método findBoyerMoore

--- src/PatternMatching.java
+++ src/PatternMatching.java
@@ -1,5 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class PatternMatching {
+
+    //Brute-Force algorithm
     public static int findBrute(char[] text, char[] pattern) {
         int n = text.length;
         int m = pattern.length;
@@ -15,6 +19,34 @@ public class PatternMatching {
         return -1;
     }
 
+    //Boyer-Moore algorithm
+    public static int findBoyerMoore(char[] text, char[] pattern) {
+        int n = text.length;
+        int m = pattern.length;
+        if (m == 0) return 0;
+        Map<Character, Integer> last = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            last.put(text[i], -1);
+        for (int k = 0; k < m; k++)
+            last.put(pattern[k], k);
+        int i = m - 1;
+        int k = m - 1;
+        while (i < n) {
+            if (text[i] == pattern[k]) {
+                if (k == 0) return i;
+                i--;
+                k--;
+            } else {
+                i += m - Math.min(k, 1 + last.get(text[i]));
+                k = m - 1;
+            }
+        }
+        return -1;
+    }
+
+
+
+    //KnutsMorris-Pratt algorithm
     public static int findKMP(char[] text, char[] pattern) {
         int n = text.length;
         int m = pattern.length;
